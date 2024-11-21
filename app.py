d0b7ea4b277f0cae72b351b35ba706aff9693cac
davazumi <davlet.khachemizov@mail.ru>
2024-11-21 16:13:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,72 +1,72 @@
-def text_calculator(expression):
-    """Функция для вычисления арифметических выражений, вводимых на русском языке."""
-    words_to_numbers = {
-        'ноль': 0, 'один': 1, 'два': 2, 'три': 3, 'четыре': 4, 'пять': 5,
-        'шесть': 6, 'семь': 7, 'восемь': 8, 'девять': 9, 'десять': 10,
-        'одиннадцать': 11, 'двенадцать': 12, 'тринадцать': 13,
-        'четырнадцать': 14, 'пятнадцать': 15, 'шестнадцать': 16,
-        'семнадцать': 17, 'восемнадцать': 18, 'девятнадцать': 19,
-        'двадцать': 20, 'тридцать': 30, 'сорок': 40, 'пятьдесят': 50,
-        'шестьдесят': 60, 'семьдесят': 70, 'восемьдесят': 80,
-        'девяносто': 90, 'сто': 100, 'двести': 200, 'триста': 300,
-        'четыреста': 400, 'пятьсот': 500, 'шестьсот': 600, 'семьсот': 700,
-        'восемьсот': 800, 'девятьсот': 900, 'тысяча': 1000,
-    }
-    
-    # Словарь для комбинирования чисел
-    composite_numbers = []
-    
-    # Генерация комбинированных чисел от 1 до 9999
-    for i in range(10000):
-        num_word = []
-        if i >= 1000:
-            num_word.append(words_to_numbers['тысяча'])
-            i -= 1000
-        if i >= 100:
-            hundreds = (i // 100) * 100
-            num_word.append(next(word for word, num in words_to_numbers.items() if num == hundreds))
-            i %= 100
-        if i >= 20:
-            tens = (i // 10) * 10
-            num_word.append(next(word for word, num in words_to_numbers.items() if num == tens))
-            i %= 10
-        if i > 0:
-            num_word.append(next(word for word, num in words_to_numbers.items() if num == i))
-        
-        composite_numbers.append(' '.join(num_word) if num_word else words_to_numbers['ноль'])
-    
-    # Замены для математики
-    expression = expression.replace('минус', '-')
-    expression = expression.replace('плюс', '+')
-    expression = expression.replace('умножить на', '*')
-    expression = expression.replace('скобка открывается', '(')
-    expression = expression.replace('скобка закрывается', ')')
+def function(calc):
+    """ Функция, принимающая строку от пользователя и возвращающая строку """
+    result = None
+    list_letter = ['ноль', 'один', 'два', 'три', 'четыре', 'пять', 'шесть',
+                   'семь', 'восемь', 'девять', 'десять', 'одиннадцать',
+                   'двенадцать', 'тринадцать', 'четырнадцать', 'пятнадцать',
+                   'шестнадцать', 'семнадцать', 'восемнадцать', 'девятнадцать']
+    list_ten = ['двадцать', 'тридцать', 'сорок', 'пятьдесят', 'шестьдесят',
+                'семьдесят', 'восемьдесят', 'девяносто']
+    list_hundred = ['сто', 'двести', 'триста', 'четыреста', 'пятьсот',
+                    'шестьсот', 'семьсот', 'восемьсот', 'девятьсот']
+    list_thousand = ['тысяча', 'две тысячи', 'три тысячи', 'четыре тысячи',
+                     'пять тысяч', 'шесть тысяч', 'семь тысяч', 'восемь тысяч',
+                     'девять тысяч']
+    list_num = []
 
-    # Обработка текста в числа
-    for word, num in words_to_numbers.items():
-        if word in expression:
-            expression = expression.replace(word, str(num))
+    for ten in list_ten:
+        for num in [''] + list_letter[1:10]:
+            if num != '':
+                list_letter.append(ten + ' ' + num)
+            else:
+                list_letter.append(ten + num)
 
+    for hundred in list_hundred:
+        for ten in [''] + list_letter[1:100]:
+            if ten != '':
+                list_letter.append(hundred + ' ' + ten)
+            else:
+                list_letter.append(hundred + ten)
+
+    for thousand in list_thousand:
+        for hundred in [''] + list_letter[1:1000]:
+            if hundred != '':
+                list_letter.append(thousand + ' ' + hundred)
+            else:
+                list_letter.append(thousand + hundred)
+
+    for num in range(0, 10000):
+        list_num.append(num)
+
+    if '  ' in calc or calc[0] == ' ' or calc[-1] == ' ':
+        print('Ошибка ввода')
+        return True
+    calc = calc.replace('минус', '-')
+    calc = calc.replace('плюс', '+')
+    calc = calc.replace('умножить на', '*')
+    calc = calc.replace('скобка открывается', '(')
+    calc = calc.replace('скобка закрывается', ')')
+    for indx, num in enumerate(reversed(list_letter[0:100])):
+        calc = calc.replace(num, str(list_num[100 - indx - 1]))    
     try:
-        # Выполнить вычисление
-        result = eval(expression)
-    except (SyntaxError, NameError):
-        print('Произошла ошибка при обработке выражения!')
-        return
+        result = eval(calc)
+    except SyntaxError:
+        print('Ошибка ввода')
+    except NameError:
+        print('Ошибка ввода')
+    
+    if result != None:
+        calc = calc.replace(' ', '')
+        if ('**' in calc or '++' in calc or calc[0] == '+' or '++' in calc or
+            '---' in calc or '-+' in calc or '+--' in calc or '*--' in calc or
+            calc.find('--') == 0):
+            print('Ошибка ввода')
+            return True
+        else:
+            try:
+                print(list_letter[list_num.index(result)])
+            except ValueError:
+                print('Ошибка ввода')
 
-    # Проверка результата
-    if abs(result) > 9999:
-        print('Ошибка: результат превышает допустимый предел!')
-    else:
-        print('Результат:', composite_numbers[result])
-
-# Приветственное сообщение
-print('''Добро пожаловать в текстовый калькулятор! 
-Вы можете выполнять операции с целыми числами, включая отрицательные, 
-скобки и неограниченное количество операций. Убедитесь, что результат 
-не превышает 9999 по модулю.''')
-
-# Ввод выражения пользователем
-text_input = input('Введите арифметическое выражение: ')
-text_calculator(text_input)
-input('Нажмите Enter для выхода')
+function(input('Введите выражение: '))
+input('Введите Enter для выхода')
